532436b7c3ca79b579a5ee1580e3e897024e62f9
birewallg <g.sstrav@gmail.com>
2023-10-08 22:18:31 +0300

    - add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module io.github.uniclog.jsticky {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires java.desktop;
+
+    opens io.github.uniclog.jsticky to javafx.fxml;
+    exports io.github.uniclog.jsticky;
+    exports io.github.uniclog.jsticky.control;
+    opens io.github.uniclog.jsticky.control to javafx.fxml;
+}
