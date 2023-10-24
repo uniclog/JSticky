3bcabf4fff73d04d8fdeee2f7b5c0d5ff9e4256f
birewallg <g.sstrav@gmail.com>
2023-10-25 00:35:10 +0300

    - add modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,12 +4,14 @@ module io.github.uniclog.jsticky {
 
     requires java.desktop;
     requires com.google.gson;
+    requires lombok;
 
     opens io.github.uniclog.jsticky.model to com.google.gson;
+    exports io.github.uniclog.jsticky.model;
     opens io.github.uniclog.jsticky to javafx.fxml;
     exports io.github.uniclog.jsticky;
-    exports io.github.uniclog.jsticky.control;
-    opens io.github.uniclog.jsticky.control to javafx.fxml;
-    exports io.github.uniclog.jsticky.control.services;
-    opens io.github.uniclog.jsticky.control.services to javafx.fxml;
+    exports io.github.uniclog.jsticky.controllers;
+    opens io.github.uniclog.jsticky.controllers to javafx.fxml;
+    exports io.github.uniclog.jsticky.controllers.services;
+    opens io.github.uniclog.jsticky.controllers.services to javafx.fxml;
 }
