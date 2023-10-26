404a7d9fb6449c488fc0cc9d33ccf7b2c33ef0ff
birewallg <g.sstrav@gmail.com>
2023-10-26 20:29:59 +0300

    - refactor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,12 +6,13 @@ module io.github.uniclog.jsticky {
     requires com.google.gson;
     requires lombok;
 
-    opens io.github.uniclog.jsticky.model to com.google.gson;
     exports io.github.uniclog.jsticky.model;
-    opens io.github.uniclog.jsticky to javafx.fxml;
     exports io.github.uniclog.jsticky;
     exports io.github.uniclog.jsticky.controllers;
-    opens io.github.uniclog.jsticky.controllers to javafx.fxml;
     exports io.github.uniclog.jsticky.controllers.services;
+
+    opens io.github.uniclog.jsticky.model to com.google.gson;
+    opens io.github.uniclog.jsticky.controllers to javafx.fxml;
+    opens io.github.uniclog.jsticky to javafx.fxml;
     opens io.github.uniclog.jsticky.controllers.services to javafx.fxml;
 }
