308ac9496b7634eddedb745b7a3fabe3b238ead2
Denis V. <g.sstrav@gmail.com>
2025-02-20 01:29:38 +0300

    - add opacity mouse listeners

--- src/main/java/io/github/uniclog/jsticky/controllers/ControllersInterface.java
+++ src/main/java/io/github/uniclog/jsticky/controllers/ControllersInterface.java
@@ -5,6 +5,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static io.github.uniclog.jsticky.App.jStickyData;
+
 public interface ControllersInterface {
     SceneControlService controlService = new SceneControlService();
     void setStageAndSetupListeners(Parent root, Scene scene, Stage stage);
@@ -13,5 +15,18 @@ public interface ControllersInterface {
         root.setOnMouseMoved(mouseEvent -> controlService.setOnMouseMoved(mouseEvent, scene));
         root.setOnMousePressed(mouseEvent -> controlService.setOnMousePressed(mouseEvent, root, stage));
         root.setOnMouseDragged(mouseEvent -> controlService.setOnMouseDragged(mouseEvent, scene, stage));
+    }
+
+    default void addMouseHoverOpacityListeners(Scene scene, Stage stage) {
+        scene.setOnMouseEntered(ev -> {
+            if (jStickyData.getWindowSettings().isMouseHoverOpacity()) {
+                stage.setOpacity(1.0D);
+            }
+        });
+        scene.setOnMouseExited(ev -> {
+            if (jStickyData.getWindowSettings().isMouseHoverOpacity()) {
+                stage.setOpacity(jStickyData.getWindowSettings().getOpacity());
+            }
+        });
     }
 }
