a7acf8c3551c44bda055a8bbed61571697ff911a
Denis V. <g.sstrav@gmail.com>
2025-02-20 01:28:40 +0300

    - add opacity options

--- src/main/java/io/github/uniclog/jsticky/model/JStickyWindowSettings.java
+++ src/main/java/io/github/uniclog/jsticky/model/JStickyWindowSettings.java
@@ -10,16 +10,31 @@ public class JStickyWindowSettings {
     private Double posX = 0D;
     private Double posY = 0D;
 
+    private Double opacity = 0.5D;
+    private Boolean mouseHoverOpacity = false;
+
+    /// Use in safe timer - dynamic parameters
     public boolean isModify(Double width, Double heigth, Double posX, Double posY) {
         return !this.width.equals(width) || !this.height.equals(heigth)
                 || !this.posX.equals(posX) || !this.posY.equals(posY);
     }
 
-    public void modifySettings(Double width, Double heigth, Double posX, Double posY) {
+    public void modifySettings(Double width, Double height, Double posX, Double posY) {
         this.width = width;
-        this.height = heigth;
+        this.height = height;
         this.posX = posX;
         this.posY = posY;
+    }
+
+    public Double getOpacity() {
+        if (opacity == null || opacity < 0.004D || opacity > 1.0D) {
+            opacity = 1.0D;
+        }
+        return opacity;
+    }
+
+    public Boolean isMouseHoverOpacity() {
+        return mouseHoverOpacity;
     }
 
     public Double getPosX() {
